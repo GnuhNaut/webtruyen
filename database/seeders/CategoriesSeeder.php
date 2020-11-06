f462d0be509b3c3ec8bb3fe0e8867943abc3cdeb
gnuhnaut <hungtt@secomus.com>
2020-11-06 08:10:00 +0700

    add categoriesSeeder

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategoriesSeeder extends Seeder
 {
@@ -13,6 +14,31 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Category::insert([
+            [
+                'id' => 1,
+                'name' => 'Ngôn Tình'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Tiên Hiệp'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Kiếm Hiệp'
+            ],
+            [
+                'id' => 4,
+                'name' => 'Truyện Teen'
+            ],
+            [
+                'id' => 5,
+                'name' => 'Truyện Ma'
+            ],
+            [
+                'id' => 6,
+                'name' => 'Lịch Sử'
+            ]
+        ]);
     }
 }
